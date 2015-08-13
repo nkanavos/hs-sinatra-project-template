7f8c219d2a8859060572e9ecb558c773efab8c19
Nicholas <nkanavos@riverdale.edu>
2015-08-13 18:05:02 +0000

    katz

--- application_controller.rb
+++ application_controller.rb
@@ -7,4 +7,35 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get'/work' do
+    erb :work
+  end
+  
+  get'/school' do
+    erb :school
+  end
+  
+  get '/relationships' do
+    erb :relationships
+  end
+  
+  get '/family' do
+    erb :family
+  end
+  
+  get '/sports' do
+    erb :sports
+  end
+  
+  get '/deadlines' do
+    erb :deadlines
+  end
+  
+  get '/other' do
+    erb :other
+  end 
+  
+  get'/future' do
+    erb :future
+  end
 end
